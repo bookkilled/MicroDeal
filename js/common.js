c5c972824b057103d688ab8ee6467602f58939a3
bookkilled <bookkilled@gmail.com>
2016-12-03 09:49:28 +0800

    1203

---
+++ js/common.js
@@ -0,0 +1,13 @@
+(function(){
+'use strict';
+
+// 是否微信
+function is_weixn(){  
+    var ua = navigator.userAgent.toLowerCase();  
+    if(ua.match(/MicroMessenger/i)=="micromessenger") {  
+        return true;  
+    } else {  
+        return false;  
+    }  
+}
+})();
