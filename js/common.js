2923e37a63a4f25961cb17bc39cb2b989274d146
bookkilled <bookkilled@gmail.com>
2017-01-22 00:13:13 +0800

    弹窗

--- js/common.js
+++ js/common.js
@@ -10,8 +10,18 @@ function is_weixn(){
     }  
 }
 
-// 弹窗
+// 弹窗（提示信息）
 function showpop(msg,btname){
+	var pop = "<div class='shade'><div class='popup'><p class='content'>"+msg+"</p><a href='javascript:;' class='button'>"+btname+"</a></div></div>";
+	$('body').append(pop);
+	$('.shade , .popup').show();
+	$('.popup .button').on('click',function(){
+		$('.shade , .popup').remove();
+		$('.popup .button').off('click');
+	});
+}
+// 弹窗（返回状态）
+function showstatus(msg,btname){
 	var pop = "<div class='shade'><div class='popup'><p class='content'>"+msg+"</p><a href='javascript:;' class='button'>"+btname+"</a></div></div>";
 	$('body').append(pop);
 	$('.shade , .popup').show();
